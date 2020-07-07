4ae8ab47b4a15aa7f4f49a386630117f124c0f10
howardbdev <howarddevennish@gmail.com>
2020-07-07 16:10:56 -0400

    load dotenv first

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'bundler/setup' # make sure all gems in the Gemfile are present on this machine
+require 'dotenv/load' # I'm only putting this here because the docs say "as early as possible"
 Bundler.require # require at runtime
 # require_relative your code
 require_relative "../lib/api"
