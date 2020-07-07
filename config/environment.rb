673418878b424970efb93fe1dcdacb2caead4325
howardbdev <howarddevennish@gmail.com>
2020-07-07 15:54:59 -0400

    add bundler/setup command

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
-require "example_cli_with_zomato/version"
-
-module ExampleCliWithZomato
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# require gems
+require 'bundler/setup' # make sure all gems in the Gemfile are present on this machine
+Bundler.require # require at runtime
+# require_relative your code
+require_relative "../lib/api"
+require_relative "../lib/cli"
+# require_relative "../lib/restaurant"
