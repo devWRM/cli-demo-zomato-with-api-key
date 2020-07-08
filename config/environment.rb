21fdfe89fa8f6087c5ae698667d18e91cc6e7c38
howardbdev <howarddevennish@gmail.com>
2020-07-08 16:10:50 -0400

    add lib version to load module

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require 'dotenv/load' # I'm only putting this here because the docs say "as earl
 Bundler.require # require at runtime
 
 # require_relative your code
+require_relative "../lib/version"
 require_relative "../lib/api"
 require_relative "../lib/cli"
 require_relative "../lib/restaurant"
