36cea1788d3c045538ae3f7e96864636587c0c7f
howardbdev <howarddevennish@gmail.com>
2020-07-08 13:51:23 -0400

    build basic restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,26 @@
-# not sure.. maybe Restaurant...
+class Restaurant
+  attr_accessor :name, :url, :address, :locality, :city, :zipcode, :cuisines, :rating
+
+  @@all = []
+
+  def initialize(attr_hash)
+    # can we refactor this code to make it cleaner?
+    @name = attr_hash[:name]
+    @url = attr_hash[:url]
+    @address = attr_hash[:address]
+    @locality = attr_hash[:locality]
+    @city = attr_hash[:city]
+    @zipcode = attr_hash[:zipcode]
+    @cuisines = attr_hash[:cuisines]
+    @rating = attr_hash[:rating]
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
