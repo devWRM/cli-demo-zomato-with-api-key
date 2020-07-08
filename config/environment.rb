2b51db6aec0bbd456ef03e7985ae565a411290bb
howardbdev <howarddevennish@gmail.com>
2020-07-08 13:50:41 -0400

    require restaurant class

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,8 @@
 require 'bundler/setup' # make sure all gems in the Gemfile are present on this machine
 require 'dotenv/load' # I'm only putting this here because the docs say "as early as possible"
 Bundler.require # require at runtime
+
 # require_relative your code
 require_relative "../lib/api"
 require_relative "../lib/cli"
-# require_relative "../lib/restaurant"
+require_relative "../lib/restaurant"
