be6cf3ebefec4e8b5f6385e07532012d5b70b8a9
howardbdev <howarddevennish@gmail.com>
2020-07-07 21:31:11 -0400

    add API#get_info - getting response now

--- lib/api.rb
+++ lib/api.rb
@@ -1,6 +1,24 @@
 # get data from the internet using scraping or hitting an API
 class API
   def get_info
-    puts "it's working from API#get_info"
+    response = HTTParty.get(
+      "https://developers.zomato.com/api/v2.1/categories",
+      {
+        headers: {
+          "Accept": "application/json",
+          "user-key": ENV["ZOMATO_API_KEY"]
+        }
+      }
+    )
+    binding.pry
   end
 end
+
+# API docs:  looks like I need "Accept" and "user-key" headers...
+# curl -X GET --header "Accept: application/json" --header "user-key: 0e40775264f643c4910dc572c757fa4e" "https://developers.zomato.com/api/v2.1/categories"
+
+# HTTParty docs: looks like the second argument is a hash with a headers key
+#response = HTTParty.get('http://example.com', {
+#   headers: {"User-Agent" => "Httparty"},
+#   debug_output: STDOUT, # To show that User-Agent is Httparty
+# })
